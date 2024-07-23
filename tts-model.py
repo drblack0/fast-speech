c5c1707652a6262a4a6ed39eb872f13fba1be1f1
drblack0 <97089794+drblack0@users.noreply.github.com>
2024-07-23 09:53:20 +0530

    the voice sample is working, uploading the voice sample for testing purpose

--- tts-model.py
+++ tts-model.py
@@ -1,11 +1,11 @@
 from TTS.api import TTS
 import torch 
-# tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=True)
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=True)
 
-# # generate speech by cloning a voice using default settings
-# tts.tts_to_file(text="It took me quite a long time to develop a voice, and now that I have it I'm not going to be silent.",
-#                 file_path="output.wav",
-#                 speaker_wav="/path/to/target/speaker.wav",
-#                 language="en")
+# generate speech by cloning a voice using default settings
+tts.tts_to_file(text="This seems like a really fun project to do, I'm halfway through it already.",
+                file_path="output.wav",
+                speaker_wav="C:\\Users\\Kartik Saxena\\Downloads\\Music\\6563_285357_000014_000000.wav",
+                language="en")
 
 print(torch.cuda.is_available())
