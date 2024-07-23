4c697189f21384e4e3ef8cc13dd0b1a88df6eca1
drblack0 <97089794+drblack0@users.noreply.github.com>
2024-07-23 10:24:39 +0530

    added requiremenets.txt

--- tts-model.py
+++ tts-model.py
@@ -5,7 +5,7 @@ tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=True)
 # generate speech by cloning a voice using default settings
 tts.tts_to_file(text="This seems like a really fun project to do, I'm halfway through it already.",
                 file_path="output.wav",
-                speaker_wav="C:\\Users\\Kartik Saxena\\Downloads\\Music\\6563_285357_000014_000000.wav",
-                language="en")
+                speaker_wav="C:\\Users\\Kartik Saxena\\Downloads\\Music\\WhatsApp-Audio-2024-07-23-at-09.53.44_3544114e.waptt.wav",
+                language="hi")
 
 print(torch.cuda.is_available())
